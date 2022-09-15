1ec54c53bea6937c88f0aed62f9341a9a29482e9
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-09-15 11:54:10 +0600

    Button click done

--- script.js
+++ script.js
@@ -124,7 +124,10 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+//startBtn.addEventListener("click", start);
+document.getElementById("starts").addEventListener("click", function () {
+
+});
 
 // If history exists, show it
 displayHistory();
